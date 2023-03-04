efa8670a045ea7d92e966cdde8b265393f08b7f2
tlepple <49572881+tlepple@users.noreply.github.com>
2023-03-04 05:34:37 -0600

    Create stream_customer_ddl.sql

---
+++ spark_items/stream_customer_ddl.sql
@@ -0,0 +1,15 @@
+CREATE TABLE <your warehouse>.<your database>.stream_customer (
+    first_name STRING,
+    last_name STRING,
+    street_address STRING,
+    city STRING,
+    state STRING,
+    zip_code STRING,
+    home_phone STRING,
+    mobile STRING,
+    email STRING,
+    ssn STRING,
+    job_title STRING,
+    create_date STRING,
+    cust_id BIGINT)
+USING iceberg;

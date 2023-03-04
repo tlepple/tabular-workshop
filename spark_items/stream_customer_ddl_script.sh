17f858d47c03040df3b9f865d1826ea7415ef02d
tlepple <49572881+tlepple@users.noreply.github.com>
2023-03-04 05:39:26 -0600

    Update stream_customer_ddl_script.sh

--- spark_items/stream_customer_ddl_script.sh
+++ spark_items/stream_customer_ddl_script.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 
+spark-sql --packages \
+  org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.1.0 \
+--properties-file /opt/spark/sql/conf.properties 
 -f /opt/spark/sql/stream_customer_ddl.sql \
 --verbose

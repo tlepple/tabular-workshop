bc847ce788ad7d9484729440a7804977c48f3135
tlepple <49572881+tlepple@users.noreply.github.com>
2023-03-05 07:51:07 -0600

    Update customer_ddl.sql

--- spark_items/customer_ddl.sql
+++ spark_items/customer_ddl.sql
@@ -1,1 +1,15 @@
-
+CREATE TABLE IF NOT EXISTS `<your warehouse>`.`<your database>`.customer (
+    first_name STRING,
+    last_name STRING,
+    street_address STRING,
+    city STRING,
+    state STRING,
+    zip_code STRING,
+    home_phone STRING,
+    mobile STRING,
+    email STRING,
+    ssn STRING,
+    job_title STRING,
+    create_date STRING,
+    cust_id BIGINT)
+USING iceberg;

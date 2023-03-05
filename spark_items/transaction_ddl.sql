2f22f0d8df7f58e4eaceb4d06b36e7b96e98e6f5
tlepple <49572881+tlepple@users.noreply.github.com>
2023-03-05 07:51:39 -0600

    Update transaction_ddl.sql

--- spark_items/transaction_ddl.sql
+++ spark_items/transaction_ddl.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE IF NOT EXISTS `<your warehouse>`.`<your database>`.transactions (
+    transact_id STRING,
+    transaction_date STRING,
+    item_desc STRING,
+    barcode STRING,
+    category STRING,
+    amount STRING,
+    cust_id BIGINT)
+USING iceberg;

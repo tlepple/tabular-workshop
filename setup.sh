e1641369255a86901d59260e464ce907724fec50
tlepple <49572881+tlepple@users.noreply.github.com>
2023-03-06 05:43:15 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -487,12 +487,12 @@ cp $TABULAR_CLIENT_FILE ~/appdist/debezium-server-iceberg/lib/
 ##########################################################################################
 sed -e "s,<your warehouse>,$TABULAR_WAREHOUSE_NAME,g" -i ~/appdist/debezium-server-iceberg/conf/application.properties
 sed -e "s,<your database>,$TABULAR_DB_NAME,g" -i ~/appdist/debezium-server-iceberg/conf/application.properties
-sed -e "s,<your credential>,$TABULAR_CREDENTIAL,g" ~/appdist/debezium-server-iceberg/conf/application.properties
+sed -e "s,<your credential>,$TABULAR_CREDENTIAL,g" -i ~/appdist/debezium-server-iceberg/conf/application.properties
 
 # setup pyiceberg
 cp ~/datagaen/.pyiceberg.yaml ~/
 sed -e "s,<your warehouse>,$TABULAR_WAREHOUSE_NAME,g" -i ~/.pyiceberg.yaml
-sed -e "s,<your credential>,$TABULAR_CREDENTIAL,g" ~/.pyiceberg.yaml
+sed -e "s,<your credential>,$TABULAR_CREDENTIAL,g" -i ~/.pyiceberg.yaml
 
 echo
 echo "---------------------------------------------------------------------"

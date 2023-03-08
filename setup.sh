08114845425360af56139f7a63ea6ba82473535c
tlepple <49572881+tlepple@users.noreply.github.com>
2023-03-08 06:25:52 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -602,8 +602,11 @@ sudo apt install nginx -y
 sudo sed -e "s,# c.JupyterHub.bind_url = 'http://:8000',c.JupyterHub.bind_url = 'http://127.0.0.1:8000/jupyter',g" -i /opt/jupyterhub/etc/jupyterhub/jupyterhub_config.py
 
 # copy the config template to the nginx service
-sudo cp ./nginx_stuff
+sudo cp ./nginx_stuff/default.conf.template /etc/nginx/sites-available/default
 
+# restart nginx
+sudo systemctl restart nginx.service
+sudo systemctl enable nginx.service
 
 
 

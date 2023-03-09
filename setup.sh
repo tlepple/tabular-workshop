3a11c2b21bf9158e8ee8119cffad14e378b85fb1
tlepple <49572881+tlepple@users.noreply.github.com>
2023-03-09 09:04:37 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -513,6 +513,7 @@ EOF
 #########################################################################################
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export SPARK_HOME=/opt/spark
+export CONDA_HOME=/opt/conda
 
 echo "" >> ~/.profile
 
